6ed66e36bd2a6d9a676db67a120b758001d12e4e
Pankaj Phour <pankaj.phour70@gmail.com>
2023-05-25 12:14:18 +0530

    Auth guard code added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
 import { MapComponent } from './map/map.component';
 import { UserDataComponent } from './user-data/user-data.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  {path:'', component:IntroComponent},
-  {path:'dashboard', component:UserDataComponent},
-  {path:'map', component:MapComponent},
+  {path:'', 
+  component:IntroComponent
+},
+  {path:'dashboard', 
+  component:UserDataComponent,
+  canActivate : [AuthGuard]
+},
+  {path:'map',
+   component:MapComponent,
+   canActivate : [AuthGuard]
+  },
+  {path:'**',
+   redirectTo : '',
+  },
 ];
 
 @NgModule({
